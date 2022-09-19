cb221a6831d73d0954ac5912faff68cb36374b5e
Habidoye97 <abidoyeabiodun97@gmail.com>
2022-09-19 17:57:59 +0100

    Add styling to create sliding navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { HashLink } from 'react-router-hash-link'
 import { NavHashLink} from 'react-router-hash-link'
 import { links } from '../data'
 import myimage from '../images/abioduntwo.png'
 import {HiOutlineMenuAlt1} from 'react-icons/hi';
+import {MdOutlineClose} from 'react-icons/md';
 import './Navbar.css'
 
 
 const Navbar = () => {
+
+  const [isShowing, NavIsShowing] = useState(false)
+
   return (
     <nav>
       <div className='container nav_container'>
@@ -17,18 +21,23 @@ const Navbar = () => {
             <h4>MY <br /> PORTFOLIO</h4>
           </div>
         </HashLink>
-        <ul>
-          {
-            links.map((link, index) => {
-              return (
-                <li key={index}>
-                  <NavHashLink to={link.path}> {link.name}</NavHashLink>
-                </li>
-              )
-            })
-          }
-        </ul>
-        <button className='btn toggleBtn'>{HiOutlineMenuAlt1}</button>
+        <div className={`nav_links-container  ${isShowing? 'show_nav open':'hide_nav'}`}>
+          <ul className='nav_links' >
+            {
+              links.map((link, index) => {
+                return (
+                  <li key={index}>
+                    <NavHashLink to={link.path}> {link.name}</NavHashLink>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </div>
+        
+        <button className='btn toggleBtn' onClick={() => NavIsShowing(!isShowing)} > 
+          { isShowing?  <MdOutlineClose /> : <HiOutlineMenuAlt1 />}
+        </button>
       </div>
     </nav>
   )
