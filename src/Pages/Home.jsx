17bc9f04177b5dacb92ee959a5d896317a92af25
Habidoye97 <abidoyeabiodun97@gmail.com>
2022-09-20 17:55:03 +0100

    change styles in home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
             <h2>Hello, I'm Abiodun Abidoye</h2>
             <h2>I'm glad you are here</h2>
             <p>
-              I'm an experienced FullStack Web Developer with technical understanding of technologies like ReactJS, Javascript, Flask, Python, SQLAlchemy. My goal is to use software engineering principles to help organizations change the world, one line of code at a time. I'm currently available for new opportunities and challenges to commit my skillsets to a worthy cause.
+              I'm an experienced FullStack Web Developer. My goal is to use software engineering principles to help organizations change the world, one line of code at a time. I'm currently available for new opportunities and challenges to commit my skillsets to a worthy cause.
             </p>
             <div className='home_section-social'>
               <h4>Connet with me!!!</h4>
@@ -25,16 +25,15 @@ const Home = () => {
             </div>
           </div>
           <div className="home_header_details-right">
-            <div className="home_header-circle"></div>
+            <div className="home_header-circle">
+              <img className='home_header-image' src={Image} alt="Main Header"/>
+            </div>
             <div className='border top-right'></div>
             <div className='border bottom-left'></div>
-            <div className="home_header-image">
-              <img src={Image} alt="Main Header"/>
-            </div>
+            
           </div>
         </div>
-      </div>
-      
+      </div> 
     </section>
   )
 }
