aeda5146bf81c0fcf619d186715df251232942c4
Habidoye97 <abidoyeabiodun97@gmail.com>
2022-09-21 17:32:14 +0100

    Add more data and update my about counponent

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,10 +1,53 @@
 import React from 'react'
 import './about.css'
+import Image from '../images/abioduntwo.png'
+import {SiLinkedin} from 'react-icons/si';
+import {FaFacebookSquare} from 'react-icons/fa';
+import {FaTwitterSquare} from 'react-icons/fa';
+import {BsArrowDownCircle} from 'react-icons/bs';
+import CV from '../assets/Abiodun_Abidoye_CV_new.pdf'
+import Cards from '../Components/Cards';
+import { databaseData, frameworkLibraryData, languageData, testingToolsData } from '../data';
+
 
 const About = () => {
+
   return (
-    <section id='about' >
-      About
+    <section className='about section container' id='about' >
+      <h2 className='section_title'>About Me</h2>
+      <span className='section_subtitle'> My Introduction </span>
+      <div className='about_img_container'>
+        <img src={Image} alt="About" className='about_img' />
+      </div>
+      <div className="about_container grid">
+        <div className='about_container-details'>
+          
+          <div className='about_data'>
+            <p className='about_description'>
+              I'm a <strong>FullStact Web Developer</strong>  with experience and technical knowledge of HTML, CSS, JavaScript, 
+              ReacJS, Python, Flask, SQLAlchemy ORM, PostgreSQL etc. I can work
+              as a <strong>Frontend Developer(ReactJS)</strong>  and as a <strong> Backend Developer(Flask).</strong>  I am passionate about learning and 
+              I'd love to use technology to improve the world.
+            </p>
+          </div>
+          <div className='about_contact'>
+            <h1>LET CONNECT</h1>
+            <div className='social'>
+                <a href="https://www.linkedin.com/in/abiodun-abidoye-59b30a143/" target='_blank' rel='noreferrer'> <SiLinkedin />  </a>
+                <a href="https://www.linkedin.com/in/abiodun-abidoye-59b30a143/" target='_blank' rel='noreferrer'> <FaFacebookSquare />  </a>
+                <a href="https://www.linkedin.com/in/abiodun-abidoye-59b30a143/" target='_blank' rel='noreferrer'> <FaTwitterSquare />  </a>
+            </div>
+            <button className='cv_button'> <a download='cv.pdf' href={CV} >
+              <h3>GET MY RESUME<BsArrowDownCircle/></h3> </a></button>
+          </div>
+        </div>
+        <div className='about_container skills'>
+          <Cards id='headingOne' datas={languageData} name='Languages'/>
+          <Cards id='headingtwo' datas={frameworkLibraryData} name='Framework & Library'/>
+          <Cards id='headingthree' datas={databaseData} name='CVS & Database'/>
+          <Cards id='headingfour' datas={testingToolsData} name='Testing Tools'/>
+        </div> 
+      </div>
     </section>
   )
 }
--- src/data.js
+++ src/data.js
@@ -9,10 +9,10 @@ export const links = [
       name: "About",
       path: '/#about'
   },
-  {
-    name: "Skills",
-    path: "/#skills"
-  },
+  // {
+  //   name: "Skills",
+  //   path: "/#skills"
+  // },
   {
       name: "Porfolio",
       path: "/#portfolio"
@@ -20,5 +20,107 @@ export const links = [
   {
       name: "Contact",
       path: '/#contact'
+  }
+]
+
+export const languageData = [
+  {
+    name: 'HTML5', 
+    image: require('./images/htmllogo.png'), 
+    description: 'HTML Logo'
+  },
+  {
+    name: 'CSS3', 
+    image: require('./images/csslogo.png'), 
+    description: 'CSS Logo'
+  },
+  {
+    name: 'Javascript', 
+    image: require('./images/javascriptlogo.png'), 
+    description: 'Javascript Logo'
+  },
+  {
+    name: 'Typescript', 
+    image: require('./images/typescriptlogo.png'), 
+    description: 'typescript Logo'
+  },
+  {
+    name: 'Python', 
+    image: require('./images/pythonlogotwo.png'), 
+    description: 'python Logo'
+  }
+]
+
+export const frameworkLibraryData = [
+  {
+    name: 'Bootstrap', 
+    image: require('./images/Bootstrap_logo.png'), 
+    description: 'bootstrap logo'
+  },
+  {
+    name: 'TailwindCSS', 
+    image: require('./images/tailwindlogo.png'), 
+    description: 'tailwindcss logo'
+  },
+  {
+    name: 'ReactJS', 
+    image: require('./images/reactlogo.webp'), 
+    description: 'reactjs logo'
+  },
+  {
+    name: 'React Redux', 
+    image: require('./images/redux.png'), 
+    description: 'react-redux-logo'
+  },
+  {
+    name: 'Flask', 
+    image: require('./images/flask.png'), 
+    description: 'flask-logo'
+  },
+  {
+    name: 'Flask-SQLAlchemy', 
+    image: require('./images/flasksqllogo.png'), 
+    description: 'python Logo'
+  }
+]
+
+export const databaseData = [
+  {
+    name: 'Netlify', 
+    image: require('./images/netlify.png'), 
+    description: 'netlify logo'
+  },
+  {
+    name: 'Kubernetes', 
+    image: require('./images/kuberneteslogo.png'), 
+    description: 'kubernetes-logo'
+  },
+  {
+    name: 'PostgreSQL', 
+    image: require('./images/postgresqllogo.png'), 
+    description: 'postgresql-logo'
+  },
+  {
+    name: 'Firebase', 
+    image: require('./images/firebaselogo.png'), 
+    description: 'react-redux-logo'
+  },
+  {
+    name: 'Git', 
+    image: require('./images/gitlogo.png'), 
+    description: 'react-redux-logo'
+  }
+]
+
+export const testingToolsData = [
+  {
+    name: 'ESLint',
+    image: require('./images/eslintlogo.png'),
+    description: 'eslint-logo'
+  },
+  {
+    name: 'Jest',
+    image: require('./images/jestlogo.svg'),
+    description: 'jest-logo'
   }
 ]
